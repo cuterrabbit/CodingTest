6759fd94d5c439acfb31f463636d65f31d0ea0da
이동욱 <122732599+cuterrabbit@users.noreply.github.com>
2026-09-19 11:27:15 +0900

    [level 2] Title: 올바른 괄호, Time: 9.00 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,22 +1,26 @@
 def solution(s):
+    s_list = list(s)
     
-    stack = []
-    
-    for p in s:
-        stack.append(p)
-    
-    sum = 0
-    
-    while len(stack) > 0:
-        if stack.pop() == ')':
-            sum += 1
-        else:
-            sum -= 1
-        
-        if sum < 0:
-            return False
-    
-    if sum == 0:
-        return True
-    else:
+    # top check
+    if s_list[-1] == '(':
         return False
+    
+    # pair check
+    n, m = 0, 0
+    for str in s_list:
+        if str == '(':
+            n += 1
+        else:
+            m += 1
+    if n != m:
+        return False
+    
+    # 올바른 괄호
+    stack = []
+    for str in s_list:
+        if str == '(':
+            stack.append(')')
+        elif not stack or stack.pop() != str:
+            return False
+        
+    return not stack
