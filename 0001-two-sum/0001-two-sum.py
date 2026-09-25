ff0ec41b1f70fc16ca15e4ca635b404f1b0254d6
이동욱 <122732599+cuterrabbit@users.noreply.github.com>
2026-09-25 16:56:54 +0900

    Time: 1741 ms (17.08%), Space: 20 MB (65.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -11,4 +11,26 @@ class Solution:
                     return [i,j]
         
         return False
+
+
+    def twoSum1(self, nums: list[int], target: int) -> list[int]:
+
+        if len(nums) == 2:
+            return [0,1]
         
+        dict_nums={}
+        for i,n in enumerate(nums):
+            if n not in dict_nums.keys():
+                dict_nums[n] = []
+            dict_nums[n].append(i)
+        
+        for num in dict_nums.keys():
+            need = target - num
+            if need in dict_nums.keys():
+                if need == num and len(dict_nums[num]) >= 2:
+                    return[int(dict_nums[num][0]),
+                     int(dict_nums[num][1])
+                     ]
+                
+                elif need != num:
+                    return [dict_nums[num][0], dict_nums[need][0]]
